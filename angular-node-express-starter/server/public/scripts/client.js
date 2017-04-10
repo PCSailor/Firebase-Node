1e2a9b6de06152898d46795f04f45b2807bee805
Phil Curtis <Phil@PhilCurtis.io>
2017-04-09 21:30:44 -0500

    logIn and logOut functions operational

--- angular-node-express-starter/server/public/scripts/client.js
+++ angular-node-express-starter/server/public/scripts/client.js
@@ -12,13 +12,23 @@ self.logIn = function(){
   console.log('login is clicked');
   auth.$signInWithPopup("google").then(function(firebaseUser){
   // .then is a promise passing back a firebase user as a detailed object
-  console.log('client.js/ firebaseUser authenticated as: ', firebaseUser.user);
-  
+  // console.log('client.js / firebaseUser authenticated as: ', firebaseUser.user);
+  console.log('client.js / firebaseUser authenticated as: ', firebaseUser.user.email);
+  }).catch(function(error){
+    console.log('client.js / firebaseUser authentication failed: ', error);
   });
-
+};
+self.logOut = function(){
+  auth.$signOut().then(function(){
+    console.log('client.js / firebaseUser logged out');
+  });
 }
 
-});
+
+
+}); // Note: FOR: app.controller
+
+
 console.log(' ');
 console.log(new Date().getFullYear() + ' server/client.js is run');
 // Macintosh HD/Users/PC/Desktop/Firebase-Explained-Lecture-Demos/angular-node-express-starter/node_modules/angular/angular.js
